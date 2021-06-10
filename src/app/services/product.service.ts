b0543a5b5840ecedc9fcf10dc99e7c978432fbb3
ProPanesar <panesar.pb08@gmail.com>
2021-06-10 00:22:54 -0400

    product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Product } from '../interfaces/product';
 import { data } from './data';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
 
-  products$: Observable<Product[]>;
+  all$: Observable<Product[]> = of(data);
 
-  constructor() { 
-    this.products$ = of(data);
-  }
+  menClothing$: Observable<Product[]> = this.all$.pipe(
+    map(products => products.filter(p => p.collectionName === 'men clothing')),
+  )
   
+  womenClothing$: Observable<Product[]> = this.all$.pipe(
+    map(products => products.filter(p => p.collectionName === 'women clothing')),
+  )
+  
+  jewelery$: Observable<Product[]> = this.all$.pipe(
+    map(products => products.filter(p => p.collectionName === 'jewelery')),
+  )
+  
+  electronics$: Observable<Product[]> = this.all$.pipe(
+    map(products => products.filter(p => p.collectionName === 'electronics')),
+  )
+
 }
